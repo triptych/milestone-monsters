25b39acfbc5c2dbd4c5f7503a8ba4fcc8b5d5e3b
triptych <triptych@gmail.com>
2010-08-26 12:03:14 -0700

    new js

---
+++ src/js/mm.js
@@ -0,0 +1,21 @@
+
+var mm = {
+		init: function(){
+			console.log("mm init!");
+		},
+		bindings: function(){
+			
+		},
+		showpanel: function(){
+			
+		}
+		
+}
+
+
+$(document).ready(function(){
+	//$("#tabs").tabs();
+	
+	//alert("hi")
+	mm.init();
+	})
