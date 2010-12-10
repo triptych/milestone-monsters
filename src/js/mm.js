b62873785dcb3d213a12876e1b945213537c873f
triptych <triptych@gmail.com>
2010-12-10 15:23:50 -0800

    Getting some animation to work

--- src/js/mm.js
+++ src/js/mm.js
@@ -1,12 +1,18 @@
 /** code for milestone monsters! **/
 
-YUI().use("node", function(Y){
+YUI().use("node", "anim", function(Y){
   
   var mm = {
     init: function(){
       Y.log("mm init!");
       mm.generateBG(.3); // default opacity is .2
+      mm.bindings()
     }, 
+    bindings: function(){
+      Y.one("#startgame").on("click", function(){
+        mm.swapPanel("splash","intro");
+      });
+    },
     generateBG: function(opacity){
       if ( !! !document.createElement('canvas').getContext) {
         return false;
@@ -31,6 +37,48 @@ YUI().use("node", function(Y){
       //document.body.style.backgroundImage = "url(" + canvas.toDataURL("image/png") + ")";
       Y.one(".viewport").setStyle("backgroundImage", "url(" + canvas.toDataURL("image/png") + ")" );
 
+    },
+    swapPanel: function(oldId, newId){
+      //put unhidden panel away
+      Y.log(oldId)
+      
+      hidingPanel = Y.one("#"+oldId);
+      var animHidePanel = new Y.Anim({
+        node: hidingPanel,
+        to: {
+          left: -400 
+        },
+        easing: Y.Easing.backIn
+      });
+      
+      var onEnd = function(){
+        hidingPanel.addClass("hide").removeClass("show").setAttribute("style","");
+        Y.log("hid one! " + oldId)
+        animShowPanel.run();
+      }
+      
+      animHidePanel.once("end", onEnd);
+      
+      animHidePanel.run();
+      
+      
+      var showingPanel = Y.one("#"+newId)
+      var animShowPanel = new Y.Anim({
+        node: showingPanel,
+        to: {
+          left: 14
+        },
+        easing: Y.Easing.backOut
+      })
+      
+      var onShowEnd = function(){
+        showingPanel.addClass("show").removeClass("hide").setAttribute("style","");
+        Y.log("shown one! " + newId );
+        
+      }
+      animShowPanel.once("end", onShowEnd);
+      
+      
     }
     
     
